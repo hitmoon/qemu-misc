2c8953d0a61dda7cdc952c9debaadb910424c0d4
hitmoon <zxq_yx_007@163.com>
2013-08-09 17:43:05 +0800

    add comment to flip.h

--- hw/flip.h
+++ hw/flip.h
@@ -1,6 +1,8 @@
 
-/* a demo device */
-/* how to use QOM */
+/* a demo device 
+* how to use qdev and QOM 
+* www.i-soft.com.cn 
+*/
 
 #ifndef HW_FLIP_H
 #define HW_FLIP_H
@@ -10,25 +12,25 @@
 #include "sysemu/sysemu.h"
 #include "exec/memory.h"
 
-#define FLIP_REG_LEN   4
+#define FLIP_REG_LEN   4    /* 32 bits register */
 
 typedef struct FLIPState{
-	uint8_t conf;
-	uint8_t state;
-	uint64_t fliped_nr;
+	uint8_t conf;          /* configuration reg */
+	uint8_t state;         /* state reg */
+	uint64_t fliped_nr;    /* total character fliped */
 
-	uint8_t in_nr;
-	uint8_t out_nr;
+	uint8_t in_nr;         /* bytes count of input reg */
+	uint8_t out_nr;        /* bytes count of output reg */
 
-	uint8_t in[FLIP_REG_LEN];
-	uint8_t out[FLIP_REG_LEN];
+	uint8_t in[FLIP_REG_LEN];   /* input reg */
+	uint8_t out[FLIP_REG_LEN];  /* output reg */
 
-	MemoryRegion io;
-	qemu_irq irq;
+	MemoryRegion io;       /* ioport used */
+	qemu_irq irq;          /* irq used */
 
-	QemuMutex lock;
+	QemuMutex lock;        /* write lock */
 
-	struct QEMUTimer *flip_timer;
+	struct QEMUTimer *flip_timer;   /* dispatch timer */
 }FLIPState;
 
 typedef struct PCIFLIPState {
@@ -36,6 +38,6 @@ typedef struct PCIFLIPState {
 	FLIPState state;
 }PCIFLIPState;
 
-extern const MemoryRegionOps flip_io_ops;
+extern const MemoryRegionOps flip_io_ops; /* io read / write functions */
 
 #endif
